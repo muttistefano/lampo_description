620f1a1689849a366009bb81d98ea48ffc7cedb8
Stefano Mutti <stefano.mutti@supsi.ch>
2024-01-06 13:13:03 +0100

    fix tf diff

--- launch/lampo_gz_mm.launch.py
+++ launch/lampo_gz_mm.launch.py
@@ -332,7 +332,7 @@ def generate_launch_description():
                 [os.path.join(get_package_share_directory('ros_gz_sim'),
                               'launch', 'gz_sim.launch.py')]),
             launch_arguments={
-                'gz_args': [' -r -v 4 ' + world_path ],
+                'gz_args': [' -r -v 1 ' + world_path ],
                 'gz_version': "8"
             }.items())
             ]
